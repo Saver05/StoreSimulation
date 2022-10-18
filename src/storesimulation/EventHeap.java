c4a0a8efd35363e3bdd97511af4e5af2e6caa703
Saver05 <89216492+Saver05@users.noreply.github.com>
2022-10-18 18:18:48 -0400

    Finished add with appropriate needed methods

--- src/storesimulation/EventHeap.java
+++ src/storesimulation/EventHeap.java
@@ -37,13 +37,61 @@ class EventHeap{
      * Insert an event into the heap. Events are ordered based on the time of the event.
      * @param item 
      */
-    public void add(Event item) { 
+    public void add(Event item)
+    {
+        if (currentSize >= CAPACITY)
+        {
+            return;
+        }
+        eventHeap[++currentSize] = item;
+        int pos = currentSize;
+
+        while (eventHeap[pos].getEventTime() < eventHeap[parent(pos)].getEventTime())
+        {
+            swap(pos,parent(pos));
+            pos = parent(pos);
+        }
     }
     
     public Event peek(){
         return null;
     }
-    
+
+    /**
+     * Returns whether a specific position is a leaf or not
+     * @param pos position of the element you wish to check
+     * @return boolean
+     */
+    private boolean isLeaf(int pos)
+    {
+        if (pos > (currentSize / 2))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * returns the parent of the node in the given position
+     * @param pos position of node you wish to find parent of
+     * @return position of the parent node
+     */
+    private int parent(int pos)
+    {
+        return pos/2;
+    }
+
+    /**
+     * swaps two nodes
+     * @param pos1 position of first node
+     * @param pos2 position of second node
+     */
+    private void swap(int pos1, int pos2)
+    {
+        Event tmp = eventHeap[pos1];
+        eventHeap[pos1] = eventHeap[pos2];
+        eventHeap[pos2] = tmp;
+    }
     public String toString(){
         String s = "[";
         for (int i = 0; i < currentSize; i++) {
